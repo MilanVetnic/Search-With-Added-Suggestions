f2e9bca22f399225b9f1d6ac2613a27c38d861ff
Milan Vetnic <61905392+MilanVetnic@users.noreply.github.com>
2021-02-08 02:02:37 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+Reusable Web Components to speed up development time and achieve consistency of both visual and user-interaction design across projects. 
+
+
+## Table of contents - 
+* [General info](#genral-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+
+## General Info 
+This project is a created Search Bar with suggestions dropdown to be reused on a web page. 
+
+** Technologies
+Project created with: 
+* HTML5
+* CSS3 
+* JavaScript
+
+
+## Setup 
+Free to include anywhere in your page to fit for best functionality. 
+
+
+
---
+++ script.js
@@ -0,0 +1,67 @@
+﻿let suggestions = [
+    "Facebook",
+    "Instagram",
+    "Travel",
+    "Pictures",
+    "Products",
+    "New",
+    "Sale",
+    "News",
+    "Trends",
+    "Sign",
+    "Buy"
+];
+
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector("input");
+const suggBox = searchWrapper.querySelector(".autocom-box");
+const icon = searchWrapper.querySelector(".icon");
+let linkTag = searchWrapper.querySelector("a");
+let webLink;
+
+inputBox.onkeyup = (e)=>{
+    let userData = e.target.value; 
+    let emptyArray = [];
+    if(userData){
+        icon.onclick = ()=>{
+            webLink = "Index.html" + userData;
+            linkTag.setAttribute("href", webLink);
+            console.log(webLink);
+            linkTag.click();
+        }
+        emptyArray = suggestions.filter((data)=>{
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            return data = '<li>'+ data +'</li>';
+        });
+        searchWrapper.classList.add("active"); 
+        showSuggestions(emptyArray);
+        let allList = suggBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchWrapper.classList.remove("active");
+    }
+}
+function select(element){
+    let selectData = element.textContent;
+    inputBox.value = selectData;
+    icon.onclick = ()=>{
+        webLink = "Index.html" + selectData;
+        linkTag.setAttribute("href", webLink);
+        linkTag.click();
+    }
+    searchWrapper.classList.remove("active");
+}
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = '<li>'+ userValue +'</li>';
+    }else{
+        listData = list.join('');
+    }
+    suggBox.innerHTML = listData;
+}
